c25c03dc49c62540369563ed65a5eec82fe66fb9
donghanbing1989@gmail.com <donghanbing1989@gmail.com>
2019-12-26 23:43:54 +0000

    Rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,4 +1,9 @@
 class Rook < Piece
-	def valid_move? 
+	def valid_move?(to_x, to_y) 
+		if x_position == to_x
+			to_y =! y_position
+		elsif y_position == to_y
+			to_x =! x_position
+		end
 	end
 end
