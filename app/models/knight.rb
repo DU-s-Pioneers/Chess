b83aa3768505bac6b06865f956f800eb287d0485
Ashley Devon <ashleychanteldevon@gmail.com>
2019-12-30 21:23:37 +0000

    adding valid move for knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,18 @@
 class Knight < Piece
+  def valid_move?(x, y)
+    if  
+      x == (self.to_x + 1) && y == (self.to_y + 2) ||
+      x == (self.to_x + 1) && y == (self.to_y - 2) ||
+      x == (self.to_x + 2) && y == (self.to_y + 1) ||
+      x == (self.to_x + 2) && y == (self.to_y - 1) ||
+      x == (self.to_x - 1) && y == (self.to_y + 2) ||
+      x == (self.to_x - 1) && y == (self.to_y - 2) ||
+      x == (self.to_x - 2) && y == (self.to_y - 1) ||
+      x == (self.to_x - 2) && y == (self.to_y + 1)
+
+        true
+      else
+        false
+    end
+  end
 end
