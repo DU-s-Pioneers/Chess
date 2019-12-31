1eaaabb082f7be0c040a7f96ef127af62298b7a9
donghanbing1989@gmail.com <donghanbing1989@gmail.com>
2019-12-31 03:41:43 +0000

    Update valid move

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,10 +1,10 @@
 class Pawn < Piece
 	def valid_move?(x_dest, y_dest) 
-		x_coord == x_dest
-		if y_coord == y_dest #first_move
-			(y_dest - y_coord).abs <= 2
-		elsif y_dest > y_coord
-			(y_dest - y_coord).abs ==1
+		x_position == x_dest
+		if y_position == y_dest #first_move
+			(y_dest - y_position).abs <= 2
+		elsif y_dest > y_position
+			(y_dest - y_position).abs ==1
 		end
 	end
 end
--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,9 +1,9 @@
 class Rook < Piece
 	def valid_move?(x_dest, y_dest) 
-		if x_coord == x_dest
-			y_dest =! y_coord
+		if x_position == x_dest
+			y_dest =! y_position
 		elsif y_coord == y_dest
-			x_dest =! x_coord
+			x_dest =! x_position
 		end
 	end
 end
