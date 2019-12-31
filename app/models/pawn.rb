4a0206ec2d1530fcdbff61277a883d8da60f09be
donghanbing1989@gmail.com <donghanbing1989@gmail.com>
2019-12-31 00:31:48 +0000

    Pawn and change Rook

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,2 +1,9 @@
 class Pawn < Piece
+	def valid_move?(x_dest, y_dest) 
+		x_coord == x_dest
+		if y_coord == y_dest #first_move
+			(y_dest - y_coord).abs <= 2
+		elsif y_dest > y_coord
+			(y_dest - y_coord).abs ==1
+		end
 end
